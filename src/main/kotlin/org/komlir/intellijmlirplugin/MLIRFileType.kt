9e575c5a6fbc8b9e252f7bf23fa8b195d7165c6e
Nir Halay <nir@quantum-machines.co>
2025-07-27 22:58:24 +0300

    Adds MLIR file type icon

--- src/main/kotlin/org/komlir/intellijmlirplugin/MLIRFileType.kt
+++ src/main/kotlin/org/komlir/intellijmlirplugin/MLIRFileType.kt
@@ -7,5 +7,5 @@ object MLIRFileType : LanguageFileType(MLIRLanguage) {
     override fun getName(): String = "MLIR"
     override fun getDescription(): String = "MLIR (Multi-Level Intermediate Representation) file"
     override fun getDefaultExtension(): String = "mlir"
-    override fun getIcon(): Icon? = null // You can add an icon later
+    override fun getIcon(): Icon = MLIRIcons.FILE
 }
---
+++ src/main/kotlin/org/komlir/intellijmlirplugin/MLIRIcons.kt
@@ -0,0 +1,9 @@
+package org.komlir.intellijmlirplugin
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object MLIRIcons {
+    @JvmField
+    val FILE: Icon = IconLoader.getIcon("/icons/mlir.svg", MLIRIcons::class.java)
+}
